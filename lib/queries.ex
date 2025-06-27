0cd5f6286282afe7f3fa601e71a451436dc72924
bhougland18 <benjamin.hougland@gmail.com>
2025-06-27 18:08:34 +0000

    Updated to include custom sql snippet updates

---
+++ lib/queries.ex
@@ -0,0 +1,12 @@
+defmodule DuckCaller.Queries do
+  use AyeSQL, runner: AyeSQL.Runner.Duckdbex
+
+  defqueries("../resources/queries/core.sql")
+
+  @doc """
+  Runs an AyeSQL query with the given name and parameters.
+  """
+  def run_query(conn, query_name, params) when is_atom(query_name) do
+    apply(__MODULE__, query_name, [conn, params])
+  end
+end
