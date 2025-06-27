721d0f776194ccdc5b331c07906864811372feaa
bhougland18 <benjamin.hougland@gmail.com>
2025-06-27 18:16:58 +0000

    added io.ex

--- lib/io.ex
+++ lib/io.ex
@@ -1,6 +1,6 @@
 defmodule DuckCaller.IO do
   @doc """
-  Creates DuckDB tables from Excel Sheets.  A table will be created for each sheet.
+  Creates DuckDB tables from Excel Sheets. A table will be created for each sheet.
   """
   def from_excel!(conn, path) do
     {:ok, package} = XlsxReader.open(path)
@@ -22,17 +22,14 @@ defmodule DuckCaller.IO do
   end
 
   defp excel_sheet_to_table!(conn, path, sheet) do
-    # TODO:Make the open_options available to change
     Duckdbex.query(
       conn,
       "CREATE TABLE #{sheet} AS SELECT * FROM st_read('#{path}', layer = '#{sheet}', open_options = ['HEADERS=FORCE']);"
-      # "CREATE TABLE #{sheet} AS SELECT * FROM st_read('#{path}', layer = '#{sheet}', open_options = ['HEADERS=FORCE', 'FIELD_TYPES=STRING']);"
     )
 
     {:ok, IO.puts("#{sheet} has been loaded into the database!")}
   end
 
-  # TODO: depreciate this as it is only a one table to one worksheet approach.  Look into the Exceed library instead.
   @doc """
   Create an Excel file for a given DuckDB Table
   """
@@ -46,7 +43,6 @@ defmodule DuckCaller.IO do
 
   def from_csv_to_table(conn, file_path, table_name, headers \\ nil, startrow \\ 1) do
     try do
-      # Prepare file path based on headers and startrow
       prepared_file_path =
         case headers do
           nil ->
@@ -57,32 +53,25 @@ defmodule DuckCaller.IO do
             prepare_csv_file(file_path, headers, delimiter, startrow)
         end
 
-      # Create table from prepared CSV
       create_table_query =
         "CREATE TABLE #{table_name} AS SELECT * FROM read_csv_auto('#{prepared_file_path}');"
 
-      # Attempt to execute the query
       case Duckdbex.query(conn, create_table_query) do
         {:ok, _result} ->
-          # Assuming we don't need to return anything specific beyond the connection
           {:ok, conn}
 
         {:error, error} ->
           {:error, "Failed to execute query for #{table_name}: #{inspect(error)}"}
       end
     rescue
-      # Handle file operations errors
       File.Error -> {:error, "File operation error for #{file_path}"}
-      # Handle any other unexpected errors
       e in RuntimeError -> {:error, "Unexpected error in from_csv_to_table: #{inspect(e)}"}
     catch
-      # Catch any other type of error that might not be a RuntimeError
       :exit, reason ->
         {:error, "Process exit or system error in from_csv_to_table: #{inspect(reason)}"}
     end
   end
 
-  # Helper function to skip rows
   defp skip_rows(file_path, startrow) when startrow == 1, do: file_path
 
   defp skip_rows(file_path, startrow) do
@@ -109,26 +98,21 @@ defmodule DuckCaller.IO do
   end
 
   defp prepare_csv_file(file_path, headers, delimiter, startrow) do
-    # Get the directory from file_path
     {dir, _} = file_path |> Path.split() |> Enum.split(-1)
     prepared_dir = Path.join(dir ++ ["prepared_files"])
     File.mkdir_p!(prepared_dir)
 
-    # Construct the new file path within the prepared_files directory
     prepared_path = Path.join(prepared_dir, Path.basename(file_path, ".csv") <> "_prepared.csv")
 
     File.open!(prepared_path, [:write, :utf8], fn file ->
-      # Write headers
       IO.write(file, Enum.join(headers, delimiter) <> "\n")
 
-      # Append and clean original file contents, skipping rows if necessary
       File.stream!(file_path, [], :line)
       |> Stream.drop(startrow - 1)
       |> Stream.each(fn line ->
         cleaned_line = repair_unterminated_quotes(line, delimiter)
         IO.write(file, cleaned_line)
       end)
-      # This forces the stream to evaluate each item
       |> Stream.run()
     end)
 
